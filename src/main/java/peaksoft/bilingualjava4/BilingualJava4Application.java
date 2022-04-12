7c4aa5821cfbdcefdd12b3397c0756e319005a93
Janyl <janyl.rk@gmail.com>
2022-04-12 15:51:55 +0600

    Initial commit

---
+++ src/main/java/peaksoft/bilingualjava4/BilingualJava4Application.java
@@ -0,0 +1,13 @@
+package peaksoft.bilingualjava4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualJava4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilingualJava4Application.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/bilingualjava4/BilingualJava4ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.bilingualjava4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualJava4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
